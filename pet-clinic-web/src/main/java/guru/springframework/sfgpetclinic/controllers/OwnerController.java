75eb497e7291d90a23f01a61e331eeadc1c30d45
username-Hello <nikitos.burak@gmail.com>
2020-06-16 11:53:54 +0300

    Adding Static Resources from Spring Pet Clinic. Closes #17

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -19,7 +19,6 @@ public class OwnerController {
     public String listOwners(Model model){
 
         model.addAttribute("owners", ownerService.findAll());
-
         return "owners/index";
     }
 }
