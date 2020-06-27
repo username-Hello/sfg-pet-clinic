6c1bb09d492624c447fab54b9f1941082b2f1e56
username-Hello <nikitos.burak@gmail.com>
2020-06-27 15:15:31 +0300

    Updated vet to use JPA. Closes #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -3,6 +3,7 @@ package guru.springframework.sfgpetclinic.model;
 import java.time.LocalDate;
 
 public class Visit extends BaseEntity {
+
     private LocalDate date;
     private String description;
     private Pet pet;
